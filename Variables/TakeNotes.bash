b57fdb3baccd21ee0a7c126d5f1d02703a7bd509
Mina Abdelsayed <minahabashy@hotmail.com>
2019-10-11 15:45:47 -0700

    First two modules

---
+++ Variables/TakeNotes.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+#Get the date
+date=$(date)
+
+#Get the topic
+topic="$1"
+
+
+#File to write to
+filename="./${topic}notes.txt"
+
+#Ask user for input:
+read -p "Please enter the note: " note
+
+echo "$date: $note" >> "$filename"
+echo "Note: '$note' saved to $(realpath $filename)"
